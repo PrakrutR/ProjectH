7219fdc40aa4eacf3ec51dc14789b6f00d8f0650
mfa188 <prakrut.rajesh07@gmail.com>
2024-03-02 17:18:06 +0100

    added memory managemnt in c

--- c/memory_management.c
+++ c/memory_management.c
@@ -1,23 +1,19 @@
+#include <mpi.h>
+#include <omp.h>
+#include <cuda_runtime.h>
 #include "../include/memory_management.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdatomic.h>
-#include <stdint.h>  // Include this to define uintptr_t
-
-#ifdef __CUDACC__
-#include <cuda_runtime.h>
-#endif
+#include <stdint.h>
 
 void* aligned_alloc(size_t size) {
     void* ptr = NULL;
     #if defined(_MSC_VER)
     ptr = _aligned_malloc(size, MEM_ALIGNMENT);
-    if (!ptr) {
-        return NULL;
-    }
     #else
     if (posix_memalign(&ptr, MEM_ALIGNMENT, size) != 0) {
-        return NULL;
+        ptr = NULL;
     }
     #endif
     return ptr;
@@ -43,39 +39,134 @@ int is_aligned(void* ptr, size_t alignment) {
     return ((uintptr_t)ptr % alignment) == 0;
 }
 
-void MemoryPool_Init(MemoryPool* pool, size_t blockSize, size_t blockCount) {
+void MemoryPool_Init(MemoryPool* pool, size_t blockSize, size_t blockCount, int world_rank, int world_size) {
+    // Calculate the portion of the pool this process will initialize, accounting for load balancing
+    size_t blocksPerProcess = blockCount / world_size;
+    size_t extraBlocks = blockCount % world_size;
+    size_t startBlock = world_rank * blocksPerProcess + (world_rank < extraBlocks ? world_rank : extraBlocks);
+    size_t endBlock = startBlock + blocksPerProcess + (world_rank < extraBlocks ? 1 : 0);
+
     pool->blockSize = blockSize > sizeof(MemoryBlock) ? blockSize : sizeof(MemoryBlock);
     pool->freeList = NULL;
     atomic_flag_clear(&pool->lock);
 
-    MemoryBlock* prevBlock = NULL;
-    for (size_t i = 0; i < blockCount; ++i) {
-        MemoryBlock* block = (MemoryBlock*)aligned_alloc_zero(pool->blockSize);
-        if (!block) {
-            // Handle allocation failure by cleaning up previously allocated blocks and returning
-            MemoryPool_Destroy(pool);
-            return;
+    int allocationFailure = 0;
+
+    #pragma omp parallel
+    {
+        MemoryBlock* localFreeList = NULL;
+
+        #pragma omp for nowait
+        for (size_t i = startBlock; i < endBlock; ++i) {
+            MemoryBlock* block = (MemoryBlock*)aligned_alloc_zero(pool->blockSize);
+            if (!block) {
+                #pragma omp atomic write
+                allocationFailure = 1;  // Indicate allocation failure
+                continue;
+            }
+            block->next = localFreeList;
+            localFreeList = block;
         }
-        block->next = prevBlock;
-        prevBlock = block;
+
+        // Thread-safe linking of blocks initialized by this thread to the global free list
+        #pragma omp critical
+        {
+            MemoryBlock* last = localFreeList;
+            while (last && last->next) {
+                last = last->next;  // Find the last block in the local list
+            }
+            if (last) {
+                last->next = pool->freeList;  // Link the local list to the global list
+                pool->freeList = localFreeList;
+            }
+        }
     }
-    pool->freeList = prevBlock;
+
+    if (allocationFailure) {
+        MemoryPool_Destroy(pool);  // Clean up in case of allocation failure
+    }
 }
 
 void MemoryPool_Grow(MemoryPool* pool, size_t additionalBlocks) {
-    // Implementation from previous response
+    if (additionalBlocks == 0) return; // No need to grow if no additional blocks are requested
+
+    while (atomic_flag_test_and_set(&pool->lock)) {
+        #if !defined(_MSC_VER)
+        sched_yield(); // Yield to other threads on POSIX-compliant systems
+        #endif
+    }
+
+    MemoryBlock* lastBlock = pool->freeList;
+    if (lastBlock) {
+        // Navigate to the end of the current free list
+        while (lastBlock->next) {
+            lastBlock = lastBlock->next;
+        }
+    }
+
+    // Add new blocks to the end of the free list
+    for (size_t i = 0; i < additionalBlocks; ++i) {
+        MemoryBlock* block = (MemoryBlock*)aligned_alloc_zero(pool->blockSize);
+        if (!block) {
+            // If allocation fails, we don't undo what's already added but stop further additions
+            break;
+        }
+
+        if (lastBlock) {
+            // Append to the existing free list
+            lastBlock->next = block;
+        } else {
+            // The free list was empty, so start a new one
+            pool->freeList = block;
+        }
+
+        lastBlock = block; // Move the lastBlock pointer to the new end of the list
+    }
+
+    atomic_flag_clear(&pool->lock);
 }
 
 void* MemoryPool_Allocate(MemoryPool* pool) {
-    // Implementation from previous response
+    while (atomic_flag_test_and_set(&pool->lock)) {
+        #if !defined(_MSC_VER)
+        sched_yield(); // POSIX-compliant yield
+        #endif
+    }
+
+    if (!pool->freeList) {
+        atomic_flag_clear(&pool->lock);
+        return NULL; // Consider growing the pool here if automatic resizing is desired
+    }
+
+    MemoryBlock* block = pool->freeList;
+    pool->freeList = block->next;
+    atomic_flag_clear(&pool->lock);
+
+    return (void*)block;
 }
 
 void MemoryPool_Deallocate(MemoryPool* pool, void* ptr) {
-    // Implementation from previous response
+    while (atomic_flag_test_and_set(&pool->lock)) {
+        #if !defined(_MSC_VER)
+        sched_yield();
+        #endif
+    }
+
+    MemoryBlock* block = (MemoryBlock*)ptr;
+    block->next = pool->freeList;
+    pool->freeList = block;
+
+    atomic_flag_clear(&pool->lock);
 }
 
 void MemoryPool_Destroy(MemoryPool* pool) {
-    // Implementation from previous response
+    MemoryBlock* block = pool->freeList;
+    while (block) {
+        MemoryBlock* next = block->next;
+        aligned_free(block);
+        block = next;
+    }
+    pool->freeList = NULL;
 }
 
 #ifdef __CUDACC__
